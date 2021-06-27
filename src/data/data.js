ab52c7caa45a111061f476d8376a092b82e6f4ff
Bence Czufor <czuforbence@gmail.com>
2021-06-27 16:47:00 +0200

    Data, dummy image, delete

---
+++ src/data/data.js
@@ -0,0 +1,152 @@
+data = [
+  {
+    title: "New bracelet 2021",
+    brand: "Pandora",
+    image: "images/pandora.jpg",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    category: "Lifestyle",
+    followers: [100, 10000],
+    audience: {
+      gender: "female",
+      age: [10, 30],
+    },
+    content: [
+      { type: "Instagram posts", amount: 4 },
+      { type: "Instagram stories", amount: 3 },
+    ],
+    hashtag: "#pandoraDK",
+    brand: {
+      handle: "@pandoraDK",
+      url: "http://pandora.dk",
+    },
+    payment: {
+      payment: "2 bracelets",
+      value: "2500",
+    },
+  },
+  {
+    title: "New bracelet 2021",
+    brand: "Pandora",
+    image: "images/pandora.jpg",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    category: "Lifestyle",
+    followers: [100, 10000],
+    audience: {
+      gender: "female",
+      age: [10, 30],
+    },
+    content: [
+      { type: "Instagram posts", amount: 4 },
+      { type: "Instagram stories", amount: 3 },
+    ],
+    hashtag: "#pandoraDK",
+    brand: {
+      handle: "@pandoraDK",
+      url: "http://pandora.dk",
+    },
+    payment: {
+      payment: "2 bracelets",
+      value: "2500",
+    },
+  },
+  {
+    title: "New bracelet 2021",
+    brand: "Pandora",
+    image: "images/pandora.jpg",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    category: "Lifestyle",
+    followers: [100, 10000],
+    audience: {
+      gender: "female",
+      age: [10, 30],
+    },
+    content: [
+      { type: "Instagram posts", amount: 4 },
+      { type: "Instagram stories", amount: 3 },
+    ],
+    hashtag: "#pandoraDK",
+    brand: {
+      handle: "@pandoraDK",
+      url: "http://pandora.dk",
+    },
+    payment: {
+      payment: "2 bracelets",
+      value: "2500",
+    },
+  },
+  {
+    title: "New bracelet 2021",
+    brand: "Pandora",
+    image: "images/pandora.jpg",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    category: "Lifestyle",
+    followers: [100, 10000],
+    audience: {
+      gender: "female",
+      age: [10, 30],
+    },
+    content: [
+      { type: "Instagram posts", amount: 4 },
+      { type: "Instagram stories", amount: 3 },
+    ],
+    hashtag: "#pandoraDK",
+    brand: {
+      handle: "@pandoraDK",
+      url: "http://pandora.dk",
+    },
+    payment: {
+      payment: "2 bracelets",
+      value: "2500",
+    },
+  },
+  {
+    title: "New bracelet 2021",
+    brand: "Pandora",
+    image: "images/pandora.jpg",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    category: "Lifestyle",
+    followers: [100, 10000],
+    audience: {
+      gender: "female",
+      age: [10, 30],
+    },
+    content: [
+      { type: "Instagram posts", amount: 4 },
+      { type: "Instagram stories", amount: 3 },
+    ],
+    hashtag: "#pandoraDK",
+    brand: {
+      handle: "@pandoraDK",
+      url: "http://pandora.dk",
+    },
+    payment: {
+      payment: "2 bracelets",
+      value: "2500",
+    },
+  },
+  {
+    title: "New bracelet 2021",
+    brand: "Pandora",
+    image: "images/pandora.jpg",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    category: "Lifestyle",
+    followers: [100, 10000],
+    audience: {
+      gender: "female",
+      age: [10, 30],
+    },
+    content: [
+      { type: "Instagram posts", amount: 4 },
+      { type: "Instagram stories", amount: 3 },
+    ],
+    hashtag: "#pandoraDK",
+    brand: {
+      handle: "@pandoraDK",
+      url: "http://pandora.dk",
+    },
+    payment: {
+      payment: "2 bracelets",
+      value: "2500",
+    },
+  },
+];
